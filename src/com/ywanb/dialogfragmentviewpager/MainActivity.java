459690486b10c9897aa8a2427b5d3af64e819ec9
ywanb3 <ywanb3@gmail.com>
2013-10-23 22:47:47 +0100

    Examples for some UI designs;can be used for later desin; One dialog; another one is for account page;

--- src/com/ywanb/dialogfragmentviewpager/MainActivity.java
+++ src/com/ywanb/dialogfragmentviewpager/MainActivity.java
@@ -2,10 +2,14 @@ package com.ywanb.dialogfragmentviewpager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends SherlockFragmentActivity {
 
@@ -25,4 +29,19 @@ public class MainActivity extends SherlockFragmentActivity {
 		fTransaction.commit();
 	}
 
+	public void onClickDialog(View v) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+		builder.setTitle("TESTING");
+		LayoutInflater inflater = (LayoutInflater) this
+				.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+		View root = inflater.inflate(R.layout.dialog_list_view,
+				(ViewGroup) findViewById(R.id.root));
+
+		builder.setView(root);
+
+		AlertDialog dialog = builder.create();
+		dialog.show();
+
+	}
 }
